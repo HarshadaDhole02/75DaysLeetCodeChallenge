b25d353cabf73970570c8062898b2d5ded3abad7
HarshadaDhole02 <harshadadhole61@gmail.com>
2026-03-29 16:18:12 +0530

    Time: 0 ms (100%), Space: 9 MB (20.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,33 @@
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+           
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            else {
+                
+                if (st.empty()) return false;
+
+                char top = st.top();
+                st.pop();
+
+            
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        // Stack should be empty at end
+        return st.empty();
+    }
+};
